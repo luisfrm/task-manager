da8e47c99f2f6a401b28791d18480bfdf79d75ac
Luis Rivas <luisfrm_@outlook.com>
2024-11-26 21:26:08 -0400

    optimized how to connect to db and added in a better ways and securer env variables

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import 'dotenv/config';
 import createApp from './app.js';
 import { connectDB } from './models/db.js';
 
-createApp();
-connectDB();
+connectDB().then(() => {
+  createApp();
+}).catch((error) => {
+  console.log('Error connecting to DB: ', error);
+});
