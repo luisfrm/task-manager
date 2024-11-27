dab9ab80fffca8963ebbd04b56623b2db1ab2530
Luis Rivas <luisfrm_@outlook.com>
2024-11-26 21:24:38 -0400

    Updated styling on navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -5,32 +5,41 @@ export default function NavBar() {
 
   const {isAuthenticated, logout} = useAuth();
 
-  console.log(isAuthenticated)
-
   return (
-		<nav className="bg-zinc-700 flex justify-between py-5 px-10">
+		<nav className="bg-[#1A1A1D] flex justify-between py-5 px-10 fixed top-0 left-0 right-0 z-50">
 			<Link to="/">
-				<h1 className="text-2xl font-bold">Task Manager</h1>
+				<h1 className="text-2xl font-bold hover:scale-105 transition-all duration-100 ease-linear">Task Manager</h1>
 			</Link>
-			<ul className="flex gap-x-2">
+			<ul className="flex">
 				{isAuthenticated && (
-          <>
-					<li>
-						<Link to="/tasks">Tasks</Link>
-					</li>
-          <li className="cursor-pointer" onClick={logout}>
-            Logout
-          </li>
-          </>
+					<>
+						<li>
+							<Link className="border-b-2 border-transparent hover:border-b-white py-2 px-3 font-semibold" to="/tasks">
+								Tasks
+							</Link>
+						</li>
+						<li>
+							<Link className="cursor-pointer border-b-2 border-transparent hover:border-b-white py-2 px-3 font-semibold" to="#" onClick={logout}>
+								Logout
+							</Link>
+						</li>
+					</>
 				)}
 
 				{!isAuthenticated && (
 					<>
 						<li>
-							<Link to="/login">Login</Link>
+							<Link className="border-b-2 border-transparent hover:border-b-white py-2 px-3 font-semibold" to="/login">
+								Login
+							</Link>
 						</li>
 						<li>
-							<Link to="/register">Register</Link>
+							<Link
+								className="border-b-2 border-transparent hover:border-b-white py-2 px-3 font-semibold"
+								to="/register"
+							>
+								Register
+							</Link>
 						</li>
 					</>
 				)}
